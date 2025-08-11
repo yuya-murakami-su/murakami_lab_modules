2768f25799509d8a00894b2e9565876507d896a7
yuya-murakami-su <murakami.yuhya@cii.shizuoka.ac.jp>
2025-08-11 21:39:31 +0900

    modify bugs

--- murakami_lab_modules/__init__.py
+++ murakami_lab_modules/__init__.py
@@ -3,6 +3,7 @@ from . import neural_network
 from . import optimizer
 from . import input_generator
 from . import model_handler
+from . import callbacks
 from . import utils
 
 __version__ = '0.2.0'
