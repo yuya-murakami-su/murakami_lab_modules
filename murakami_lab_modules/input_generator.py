89afddb59b88adcda912b39629126d6b13c00add
yuya-murakami-su <murakami.yuhya@cii.shizuoka.ac.jp>
2025-12-29 15:22:30 +0900

    v0.2.5

--- murakami_lab_modules/input_generator.py
+++ murakami_lab_modules/input_generator.py
@@ -35,7 +35,7 @@ class InputGenerator:
                 raise ValueError('length of inputs must be same as size_of_generated_inputs')
             self.generator = self._fixed_distribution(inputs=kwargs['inputs'])
         else:
-            exec(f'self.generator = self._{distribution}(**kwargs)')
+            self.generator = getattr(self, f'_{distribution}')(**kwargs)
 
     def _random_distribution(self, input_range: tuple = None, **_: dict):
         if input_range is None:
